d625e5a4871cbbb0966235805f21b6e2dd9c11e2
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2023-03-30 16:40:25 +1100

    adds pointblank checks to get_records.R

--- scripts/get_records.R
+++ scripts/get_records.R
@@ -1,16 +1,7 @@
-# script to loop over records by year and download using {galah}
-# TODO: add EPBC and GRIIS once indexed, pointblank checks
+# downloads records using {galah}, iterating over years, pausing between API calls
 
-library(galah)
-library(purrr)
-library(arrow)
-library(dplyr)
-
-galah_config(email = Sys.getenv("email"), verbose = TRUE)
-
-# downloads records for selected year, with a pause between API calls
 # OR filters (currently) not working in {galah}, so manually filter IBRA/IMCRA 
-# (cl1048 IS NOT NULL OR cl966 IS NOT NULL)
+# i.e. (cl1048 IS NOT NULL OR cl966 IS NOT NULL)
 get_occ <- function(my_year) {
   
   y <- tibble(name =
@@ -71,8 +62,20 @@ get_occ <- function(my_year) {
 years <- as.numeric(c(1900:2022))
 map(years, get_occ)
 
-### tests ------
-# 1 file for every year
-#length(list.files("data/galah")) == length(years) 
-# number of columns downloaded matches selected fields
-#expect_col_count_match(tbl, count = tbl_2)
+# run checks on the downloaded data
+ds <- open_dataset("data/galah", format = "parquet")
+duck_tbl <- ds |> to_duckdb()
+
+# 01. correct number of columns
+# 02. years are within the expected range
+# 03. speciesID, lat, and lon fields don't contain blanks
+agent <- create_agent(tbl = duck_tbl,
+                      tbl_name = "get_records",
+                      label = "Check validity of get_records.R") |>
+  col_count_match(count = 33) |> 
+  col_vals_between(columns = vars(year), left = min(years), right = max(years)) |>
+  col_vals_not_null(columns = vars(decimalLatitude, decimalLongitude, speciesID)) |> 
+  interrogate()
+
+x_write_disk(agent, filename = "agent-get_records", path = "tests")  
+
