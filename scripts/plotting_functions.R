615c0105858f01dd646b4869697690271ed7274c
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2023-05-24 17:27:37 +1000

    adds code for creating plots from monitoring datasets as helper functions

--- scripts/plotting_functions.R
+++ scripts/plotting_functions.R
@@ -154,3 +154,85 @@ plot_choropleth <- function(imcra_dat, ibra_dat) {
           plot.background = element_rect(fill = 'white', colour = 'white'),
           panel.background = element_rect(fill = 'white', colour = 'white')) 
 }
+
+plot_monitoring_effort <- function(ibra_dat, imcra_dat) {
+  
+  ggplot() +
+    geom_sf(data = ibra_dat,
+            aes(fill = intensity),
+            colour = NA) +
+    scale_fill_gradient(name = "IBRA",
+                        low = "#ccd2ab",
+                        high = "#15521d",
+                        limits = c(0, 4),
+                        breaks = c(0, 1, 2, 3, 4), 
+                        guide = guide_colorbar(draw.ulim = FALSE,
+                                               draw.llim = FALSE,
+                                               ticks = FALSE,
+                                               direction = "horizontal",
+                                               label.position = "bottom",
+                                               title.position = "left",
+                                               title.vjust = 0.85,
+                                               title.hjust = 0.85)) +
+    new_scale_fill() +
+    geom_sf(data = imcra_dat,
+            aes(fill = intensity),
+            colour = NA) +
+    scale_fill_gradient(name = "IMCRA",
+                        low = "#bed7ce",
+                        high = "#286699",
+                        limits = c(0, 4),
+                        breaks = c(0, 1, 2, 3, 4), 
+                        guide = guide_colorbar(draw.ulim = FALSE,
+                                               draw.llim = FALSE,
+                                               ticks = FALSE,
+                                               direction = "horizontal",
+                                               label.position = "bottom",
+                                               title.position = "left",
+                                               title.vjust = 0.85,
+                                               title.hjust = 0.85)) +
+    coord_sf(xlim = c(110, 155),
+             ylim = c(-45, -2)) +
+    annotate("text",
+             x = 130,
+             y = -3,
+             label = "Environmental Monitoring and Observations Effort",
+             size = 8) +
+    annotate("text",
+             x = 128.5,
+             y = -5,
+             label = "Coverage by major earth science facets (2010 - 2022)",
+             size = 7) +
+    annotate("text",
+             x = 132.5,
+             y = -45.5, 
+             label = "Mean number of major earth science facets measured per year",
+             size = 6) +
+    theme_void() +
+    theme(text = element_text(family = "lato"),
+          title = element_text(face = "bold"),
+          legend.position = "bottom",
+          legend.key.width = unit(12, 'mm'),
+          plot.background = element_rect(fill = 'white', colour = 'white'),
+          panel.background = element_rect(fill = 'white', colour = 'white'))
+}
+
+plot_sample_event_points <- function(ibra_sf, imcra_sf, point_data, point_col) {
+  
+  ggplot() +
+    geom_sf(data = ibra_sf,
+            fill = "#fee5b155",
+            colour = "#fee5b1") +
+    geom_sf(data = imcra_sf,
+            fill = "#bed7ce55",
+            colour = "#bed7ce") +
+    geom_sf(data = point_data,
+            shape = 16,
+            colour = point_col,
+            size = 0.8) +
+    coord_sf(xlim = c(110, 155),
+             ylim = c(-45, -5)) +
+    theme_void() +
+    theme(plot.background = element_rect(fill = 'white', colour = 'white'),
+          panel.background = element_rect(fill = 'white', colour = 'white'))
+}
