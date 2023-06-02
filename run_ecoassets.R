fa5b148c64f2d767d32c736e2f592c8ae740d650
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2023-06-02 12:34:56 +1000

    run script for EcoAssets

--- run_ecoassets.R
+++ run_ecoassets.R
@@ -14,3 +14,10 @@ source("scripts/plot_monitoring.R")
 
 # 5. create three relational tables to generate biodiversity datasets
 source("scripts/create_relational_tables.R")
+
+# TODO:
+# 6. generate aggregated biodiversity summaries from relational tables
+source("scripts/facet_biodiversity.R")
+
+# 7. create plots from biodiversity datasets
+source("scripts/plot_biodiversity.R")
