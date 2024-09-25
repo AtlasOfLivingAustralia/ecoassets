8eaab2bb87419b665e54fda6c5d0e7decf0fe653
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2024-09-25 12:35:18 +1000

    minor edits to script for checking downloads

--- scripts/check_records.R
+++ scripts/check_records.R
@@ -4,9 +4,9 @@ source("R/checking_functions.R")
 
 ds <- open_dataset("data/galah")
 
-check_col_count(33)
+check_col_count(ds, 33)
 
-check_not_na(speciesID)
-check_not_na(year)
-check_not_na(decimalLatitude)
-check_not_na(decimalLongitude)
+check_not_na(ds, speciesID)
+check_not_na(ds, year)
+check_not_na(ds, decimalLatitude)
+check_not_na(ds, decimalLongitude)
