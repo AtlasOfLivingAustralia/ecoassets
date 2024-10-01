369305bb6b3cabb8b19016e644ae5ab9a866bea9
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2024-10-01 13:52:33 +1000

    replaces custom checking functions with functions from assertthat

--- R/checking_functions.R
+++ R/checking_functions.R
@@ -1,26 +1,10 @@
-# Checks downloaded data for common errors
-# TODO: add check to confirm no columns are entirely NULL/NA - 
-# this should safeguard against layers being disabled 
+# Custom functions to check downloaded data for common errors
 
-check_col_count <- function(ds, col_count) {
-   assert_that(
-    length(ds$schema) == col_count,
-    msg = "schema length does not match col_count"
-  )
- }
-
-check_not_na <- function(ds, col_name) {
-   assert_that(
-    sum(is.na(pull(ds, {{col_name}}, as_vector = TRUE))) == 0,
-    msg = "this column contains one or more NAs"
-  )
- }
-
-# incomplete
-check_not_all_na <- function(ds, col_name) {
+# safeguards against not noticing layers have been disabled
+not_all_NA <- function(ds, col_name) {
+  assert_that(has_name(ds, col_name))
   assert_that(
-    all(is.na(pull(ds, {{col_name}}, as_vector = TRUE))) == 0,
-    msg = "this column only contains NAs"
+    sum(is.na(pull(ds, col_name, as_vector = TRUE))) < nrow(ds), 
+    msg = "This column contains only NAs"
   )
 }
-
--- scripts/check_records.R
+++ scripts/check_records.R
@@ -4,9 +4,15 @@ source("R/checking_functions.R")
 
 ds <- open_dataset("data/galah")
 
-check_col_count(ds, 33)
+# 1. checks number of downloaded columns
+assert_that(are_equal(ncol(ds), 33))
 
-check_not_na(ds, speciesID)
-check_not_na(ds, year)
-check_not_na(ds, decimalLatitude)
-check_not_na(ds, decimalLongitude)
+# 2. checks columns for NAs
+assert_that(noNA(pull(ds, year, as_vector = TRUE)))
+assert_that(noNA(pull(ds, decimalLatitude, as_vector = TRUE)))
+assert_that(noNA(pull(ds, decimalLongitude, as_vector = TRUE)))
+assert_that(noNA(pull(ds, speciesID, as_vector = TRUE)))
+
+# 3. checks none of the columns contain only NAs
+# TODO: loop this over all columns
+assert_that(not_all_NA(ds, "cl10000"))
