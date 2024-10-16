6d04a800fe185f32df358ece27302377364c88bc
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2024-10-16 12:08:48 +1100

    minor formatting, shuffling code between scripts

--- run_ecoassets.R
+++ run_ecoassets.R
@@ -16,25 +16,21 @@
   library(httr)
   library(jsonlite)
 }
-# TODO: source functions here 
-
+# TODO: source functions here? 
 
 conflicts_prefer(
   dplyr::filter,
   tidyr::unnest
 )
 
-
-# options / environment variables ----- 
+# options, config  ----- 
 galah_config(email = Sys.getenv("ALA_EMAIL"), verbose = TRUE)
 options(arrow.pull_as_vector = TRUE)
-
-years <- as.numeric(c(1900:2023))
-
+sf_use_s2(FALSE)
 
 # get data ----- 
+years <- as.numeric(c(1900:2023))
 walk(years, get_occ)
-
 
 # check data -----
 ds <- open_dataset("data/galah")
@@ -44,10 +40,8 @@ assert_that(noNA(pull(ds, decimalLatitude)))
 assert_that(noNA(pull(ds, decimalLongitude)))
 assert_that(noNA(pull(ds, speciesID)))
 # TODO: loop this over all columns
-assert_that(not_all_NA(ds, "cl10000"))
-lapply(names(ds)[1:3], not_all_NA, ds = ds)
-
-
+# assert_that(not_all_NA(ds, "cl10000"))
+# lapply(names(ds)[1:3], not_all_NA, ds = ds)
 
 # 3. monitoring datasets and plots 
 source("scripts/assemble_monitoring.R")
