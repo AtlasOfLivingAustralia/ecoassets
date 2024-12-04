e93a6f18921737698c5f29d25b22b6eaa606cfcf
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2024-12-05 10:26:29 +1100

    moves summarising function for plotting to R/

--- R/summarise_data.R
+++ R/summarise_data.R
@@ -27,3 +27,29 @@ summarise_events <- function(fpath) {
     write_parquet(sink = paste0("data/tmp_events_agg/year_", year_id, ".parquet"))
   
 }
+
+#' Summarise proportions
+#'
+#' @param df 
+#' @param region_type 
+#' @param status_val 
+#'
+#' @export
+
+get_proportions <- function(df, region_type, status_val) {
+  
+  df |> 
+    group_by({{region_type}}, yearStart, yearEnd, status) |>
+    mutate(groupedSpeciesCount = sum(speciesCount)) |>
+    ungroup() |> 
+    distinct({{region_type}}, yearStart, yearEnd, status, groupedSpeciesCount) |> 
+    group_by({{region_type}}, yearStart, yearEnd) |>
+    mutate(totalCount = sum(groupedSpeciesCount)) |>
+    ungroup() |> 
+    filter(status == status_val) |>
+    rowwise() |>
+    mutate(prop = groupedSpeciesCount / totalCount) |>
+    ungroup() |> 
+    mutate(period = paste0(yearStart, "—", yearEnd))
+}
+
